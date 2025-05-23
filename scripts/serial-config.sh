8a8a9aaa19799f4ce1c3b3671d828ef36c321fb7
Kevin Allioli <kevin@netarchitect.cloud>
2025-05-23 21:59:31 +0200

    Build rc image

--- scripts/serial-config.sh
+++ scripts/serial-config.sh
@@ -1,8 +1,3 @@
 #!/bin/ash
-# Configuration simple de la console série pour Alpaquita Linux
-
-# Activer getty sur ttyS0 dans inittab
 sed -i "s|^#ttyS0::|ttyS0::|" /etc/inittab
-
-# Ajouter ttyS0 à securetty si pas déjà présent
 grep -q "^ttyS0$" /etc/securetty || echo "ttyS0" >> /etc/securetty
