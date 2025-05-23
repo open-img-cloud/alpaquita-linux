6c7d4c0479a5a53ed6cbcbc734be277fa6a73d7c
Kevin Allioli <kevin@netarchitect.cloud>
2025-05-23 21:00:26 +0200

    Update packages

--- scripts/clean-user.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/ash
-userdel -r alpaquita 2>/dev/null || true
-sed -i "/^alpaquita:/d" /etc/shadow /etc/passwd /etc/group 2>/dev/null || true
-rm -rf /home/alpaquita
--- scripts/serial-config.sh
+++ scripts/serial-config.sh
@@ -1,9 +1,8 @@
 #!/bin/ash
-sed -i "s/#ttyS0::respawn:\/sbin\/getty -L 115200 ttyS0 vt100/ttyS0::respawn:\/usr\/sbin\/getty -L 115200 ttyS0 vt100/" /etc/inittab
-echo "ttyS0" >> /etc/securetty
-cp /etc/init.d/agetty.tty1 /etc/init.d/getty.ttyS0
-sed -i "s/agetty.tty1/getty.ttyS0/g" /etc/init.d/getty.ttyS0
-sed -i "s/tty1/ttyS0/g" /etc/init.d/getty.ttyS0
-sed -i "s/agetty/getty -L 115200/g" /etc/init.d/getty.ttyS0
-chmod +x /etc/init.d/getty.ttyS0
-rc-update add getty.ttyS0 default
+# Configuration simple de la console série pour Alpaquita Linux
+
+# Activer getty sur ttyS0 dans inittab
+sed -i "s|^#ttyS0::|ttyS0::|" /etc/inittab
+
+# Ajouter ttyS0 à securetty si pas déjà présent
+grep -q "^ttyS0$" /etc/securetty || echo "ttyS0" >> /etc/securetty
